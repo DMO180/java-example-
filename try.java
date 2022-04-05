88af1936e93da0c6d591ff40153082fcef1ba9fe
Dhruv Moradiya <dhruv.moradiya365@gmail.com>
2022-04-05 15:07:16 +0530

    new file

---
+++ try.java
@@ -0,0 +1,19 @@
+
+// Java program to read character using Scanner 
+// class
+import java.util.Scanner;
+public class try
+{
+    public static void main(String[] args)
+    {
+        // Declare the object and initialize with
+        // predefined standard input object
+        Scanner sc = new Scanner(System.in);
+   
+        // Character input
+        char c = sc.next().charAt(0);
+   
+        // Print the read value
+        System.out.println("c = "+c);
+    }
+}
